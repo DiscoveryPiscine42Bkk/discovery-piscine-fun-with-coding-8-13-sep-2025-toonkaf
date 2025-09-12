b0eb2ef701e2d814c12a1bcaf69f53be424be1b7
Ratchanon Yaemboo <toonkaf@gmail.com>
2025-09-12 14:07:31 +0700

    c03/ex04

--- cell03/ex04/ex00bis/background.js
+++ cell03/ex04/ex00bis/background.js
@@ -2,7 +2,6 @@ function random_color(){
     var hex , color ;
     hex = '0123456789ABCDEF';
     color = '#'
-    console.log(Math.random()*16);
     for(let i = 0 ;i < 6 ; i++) color += hex[Math.floor(Math.random() * 16)];
     return color ;
 }
